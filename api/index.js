e6ca751e641e2741be6c943bed34df2ad363a005
firaflash <flexflash09@gmail.com>
2026-01-04 15:59:41 +0300

    Finalized the  project initalization inites to collaborators is sent

--- api/index.js
+++ api/index.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('../Public'))
+
+
+
 const PORT = process.env.PORT || 5000;
 
 
