e35b8573cf4468392f50c6a0840f8c8166854d76
firaflash <flexflash09@gmail.com>
2026-01-04 12:50:01 +0300

    Remove node_modules from repository

--- api/index.js
+++ api/index.js
@@ -7,25 +7,23 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-// Your routes
+
 app.get('/', (req, res) => {
-  console.log("Landing Page sent from the server");
   res.send('Hello from School Service Request Voting Platform!');
 });
 
 app.post('/vote', (req, res) => {
   const { serviceId, vote } = req.body;
-  console.log(`Vote received for service ${serviceId}: ${vote}`);
   res.json({ success: true, message: 'Vote recorded!' });
 });
 
-// Bridge for Vercel serverless (this is the key!)
-export default (req, res) => {
-  app(req, res);
-};
+export default app;
 
-app.listen(PORT,(req,res)=>{
-    console.log(`Server Running on port https://localhost:${PORT}`);
-})
+
+if(process.env.NODE_ENV !== 'production'){
+  app.listen(PORT,(req,res)=>{
+    console.log(`Server Running on port  http://localhost:${PORT}`);
+  })
+}
